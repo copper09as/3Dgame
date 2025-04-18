513386fa4adf0d11fa1a62f32832da5ce44efa53
copper09as <494699676@qq.com>
2025-04-18 23:44:01 +0800

    对话

---
+++ Assets/Script/Ui/TalkSo.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "TalkSo", menuName = "Talk/TalkSo")]
+public class TalkSo : ScriptableObject
+{
+    public List<string> talk;
+}
